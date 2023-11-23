c88c7ca2c0ad5ee1202f923079a394873e9aafa4
Dutta Sreevalli <101463564+duttasreevalli@users.noreply.github.com>
2023-11-23 12:04:35 +0530

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -1,7 +1,6 @@
 import streamlit
-import snowflake.connector
+
 streamlit.title("Zena's Amazing Athleisure Catalog")
 streamlit.text("Pick a sweatshirt color or style")
 
-sweatshirtpick=streamlit.text_input(
                 
